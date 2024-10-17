1e08cc291c570ad80437e4a30e58934e8bb35a91
vyacheslavnov03 <vyacheslavnov03@mail.ru>
2024-10-17 22:23:04 +0300

    Initial commit

---
+++ src/main/java/ru/tbank/emailcheckerbot/EmailCheckerBotApplication.java
@@ -0,0 +1,13 @@
+package ru.tbank.emailcheckerbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailCheckerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailCheckerBotApplication.class, args);
+	}
+
+}
