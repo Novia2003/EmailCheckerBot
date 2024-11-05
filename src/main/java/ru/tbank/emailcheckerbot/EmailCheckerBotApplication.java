62bc83e3a728d7ac81464eb4db156f72bd80861e
vyacheslavnov03 <vyacheslavnov03@mail.ru>
2024-11-06 01:00:42 +0300

    added configuration and properties

--- src/main/java/ru/tbank/emailcheckerbot/EmailCheckerBotApplication.java
+++ src/main/java/ru/tbank/emailcheckerbot/EmailCheckerBotApplication.java
@@ -2,8 +2,10 @@ package ru.tbank.emailcheckerbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EmailCheckerBotApplication {
 
 	public static void main(String[] args) {
