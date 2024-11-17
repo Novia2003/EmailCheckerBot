107fb6cb681c71d1ea96a76a713b9baae134cfa8
vyacheslavnov03 <vyacheslavnov03@mail.ru>
2024-11-18 01:43:20 +0300

    added mail.ru configuration and properties

---
+++ src/main/java/ru/tbank/emailcheckerbot/configuration/property/MailRuProperties.java
@@ -0,0 +1,17 @@
+package ru.tbank.emailcheckerbot.configuration.property;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties("mailru")
+public class MailRuProperties {
+    private String url;
+    private String clientId;
+    private String clientSecret;
+    private String redirectUri;
+}
