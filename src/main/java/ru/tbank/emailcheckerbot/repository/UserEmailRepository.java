c7554bf893407399174fc8e5dcbf9183b07544f1
vyacheslavnov03 <vyacheslavnov03@mail.ru>
2024-11-24 13:38:15 +0300

    added changes related to changing the class name

--- src/main/java/ru/tbank/emailcheckerbot/repository/UserEmailRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.tbank.emailcheckerbot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.tbank.emailcheckerbot.entity.UserEmailEntity;
-
-public interface UserEmailRepository extends JpaRepository<UserEmailEntity, Long> {
-}
