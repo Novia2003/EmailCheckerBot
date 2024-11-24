bd3d6c9f4f646559ec5aadf3a30422b574af57ff
vyacheslavnov03 <vyacheslavnov03@mail.ru>
2024-11-25 01:36:13 +0300

    added a description of the controllers in swagger

--- src/main/java/ru/tbank/emailcheckerbot/controller/AuthenticationController.java
+++ src/main/java/ru/tbank/emailcheckerbot/controller/AuthenticationController.java
@@ -1,5 +1,11 @@
 package ru.tbank.emailcheckerbot.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,11 +17,54 @@ import ru.tbank.emailcheckerbot.service.AuthenticationService;
 @Controller
 @RequestMapping("/api/v1/oauth")
 @RequiredArgsConstructor
+@Tag(
+        name = "AuthenticationController",
+        description = "Functions for OAuth authentication"
+)
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
 
     @GetMapping("/receiver")
+    @Operation(
+            description = "Getting the code needed for exchanging to access_token during OAuth"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successful operation",
+                            content = @Content(
+                                    mediaType = "text/html",
+                                    schema = @Schema(implementation = String.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Invalid input",
+                            content = @Content(
+                                    mediaType = "text/html",
+                                    schema = @Schema(implementation = String.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "User record not found",
+                            content = @Content(
+                                    mediaType = "text/html",
+                                    schema = @Schema(implementation = String.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "503",
+                            description = "Service unavailable",
+                            content = @Content(
+                                    mediaType = "text/html",
+                                    schema = @Schema(implementation = String.class)
+                            )
+                    )
+            }
+    )
     public String getCode(
             @RequestParam String state,
             @RequestParam String code,
--- src/main/java/ru/tbank/emailcheckerbot/controller/MessageController.java
+++ src/main/java/ru/tbank/emailcheckerbot/controller/MessageController.java
@@ -1,5 +1,11 @@
 package ru.tbank.emailcheckerbot.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,11 +17,54 @@ import ru.tbank.emailcheckerbot.service.EmailUIDService;
 @Controller
 @RequestMapping("/api/v1/messages")
 @RequiredArgsConstructor
+@Tag(
+        name = "MessageController",
+        description = "Functions for viewing email messages"
+)
 public class MessageController {
 
     private final EmailUIDService emailUIDService;
 
     @GetMapping
+    @Operation(
+            description = "Getting the full content of an email message by its UID"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successful operation",
+                            content = @Content(
+                                    mediaType = "text/html",
+                                    schema = @Schema(implementation = String.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Invalid input",
+                            content = @Content(
+                                    mediaType = "text/html",
+                                    schema = @Schema(implementation = String.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Message not found",
+                            content = @Content(
+                                    mediaType = "text/html",
+                                    schema = @Schema(implementation = String.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "503",
+                            description = "Service unavailable",
+                            content = @Content(
+                                    mediaType = "text/html",
+                                    schema = @Schema(implementation = String.class)
+                            )
+                    )
+            }
+    )
     public String getClients(
             @RequestParam Long userEmailId,
             @RequestParam Long messageUID,
