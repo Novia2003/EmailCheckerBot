269279c1ac0d766943adc4948400eed9d25b13b5
vyacheslavnov03 <vyacheslavnov03@mail.ru>
2024-11-24 13:37:16 +0300

    added a check for the lifetime of the access token

--- src/main/java/ru/tbank/emailcheckerbot/service/EmailCheckService.java
+++ src/main/java/ru/tbank/emailcheckerbot/service/EmailCheckService.java
@@ -4,10 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import ru.tbank.emailcheckerbot.entity.UserEmailEntity;
+import ru.tbank.emailcheckerbot.entity.postgre.UserEmailPostgreEntity;
 import ru.tbank.emailcheckerbot.message.EmailMessage;
-import ru.tbank.emailcheckerbot.repository.UserEmailRepository;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.ExecutorService;
@@ -17,44 +17,50 @@ import java.util.concurrent.ExecutorService;
 @RequiredArgsConstructor
 public class EmailCheckService {
 
-    private final UserEmailRepository userEmailRepository;
-
     private final ExecutorService fixedThreadPool;
     private final EmailSessionPropertiesService emailSessionPropertiesService;
     private final NotificationService notificationService;
     private final EmailSessionService emailSessionService;
+    private final UserEmailPostgreService userEmailPostgreService;
+    private final AuthenticationService authenticationService;
 
     @Scheduled(fixedDelayString = "${email.check.delay}")
     public void checkEmails() {
         log.info("Starting email check process");
 
-        List<UserEmailEntity> emails = userEmailRepository.findAll();
-        for (UserEmailEntity email : emails) {
+        List<UserEmailPostgreEntity> emails = userEmailPostgreService.getUserEmails();
+        for (UserEmailPostgreEntity email : emails) {
             fixedThreadPool.submit(() -> checkForNewMessages(email));
         }
 
         log.info("Email check process completed");
     }
 
-    private void checkForNewMessages(UserEmailEntity userEmailEntity) {
-            Properties properties = emailSessionPropertiesService.getSessionProperties(userEmailEntity.getEmailProvider());
+    private void checkForNewMessages(UserEmailPostgreEntity userEmailPostgreEntity) {
+        Properties properties = emailSessionPropertiesService.getSessionProperties(
+                userEmailPostgreEntity.getMailProvider().getConfigurationName()
+        );
 
-            EmailMessage[] newMessages = emailSessionService.getNewMessages(
-                    properties,
-                    userEmailEntity.getEmail(),
-                    userEmailEntity.getAccessToken(),
-                    userEmailEntity.getLastMessageUID()
-            );
+        if (Instant.now().isAfter(userEmailPostgreEntity.getEndAccessTokenLife())) {
+            log.info("Access token expired for user: {}. Refreshing token...", userEmailPostgreEntity.getEmail());
+            authenticationService.refreshToken(userEmailPostgreEntity.getId(), true);
+        }
 
-            if (newMessages.length > 0) {
-                log.info("New messages found for user: {}", userEmailEntity.getEmail());
-                notificationService.notifyUser(newMessages, userEmailEntity);
+        EmailMessage[] newMessages = emailSessionService.getNewMessages(
+                properties,
+                userEmailPostgreEntity.getEmail(),
+                userEmailPostgreEntity.getAccessToken(),
+                userEmailPostgreEntity.getLastMessageUID()
+        );
 
-                Long lastMessageUID = newMessages[newMessages.length - 1].getUid();
-                userEmailEntity.setLastMessageUID(lastMessageUID);
-                userEmailRepository.save(userEmailEntity);
-            } else {
-                log.info("No new messages found for user: {}", userEmailEntity.getEmail());
-            }
+        if (newMessages.length > 0) {
+            log.info("New messages found for user: {}", userEmailPostgreEntity.getEmail());
+            notificationService.notifyUser(newMessages, userEmailPostgreEntity);
+
+            Long lastMessageUID = newMessages[newMessages.length - 1].getUid();
+            userEmailPostgreService.setLastMessageUID(userEmailPostgreEntity.getId(), lastMessageUID);
+        } else {
+            log.info("No new messages found for user: {}", userEmailPostgreEntity.getEmail());
+        }
     }
 }
--- src/main/java/ru/tbank/emailcheckerbot/service/EmailUIDService.java
+++ src/main/java/ru/tbank/emailcheckerbot/service/EmailUIDService.java
@@ -2,10 +2,11 @@ package ru.tbank.emailcheckerbot.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.tbank.emailcheckerbot.entity.UserEmailEntity;
+import ru.tbank.emailcheckerbot.entity.MailProvider;
+import ru.tbank.emailcheckerbot.entity.postgre.UserEmailPostgreEntity;
 import ru.tbank.emailcheckerbot.message.EmailMessage;
-import ru.tbank.emailcheckerbot.repository.UserEmailRepository;
 
+import java.time.Instant;
 import java.util.Properties;
 
 @Service
@@ -14,17 +15,24 @@ public class EmailUIDService {
 
     private final EmailSessionPropertiesService emailSessionPropertiesService;
     private final EmailSessionService emailSessionService;
+    private final UserEmailPostgreService userEmailPostgreService;
+    private final AuthenticationService authenticationService;
 
-    private final UserEmailRepository userEmailRepository;
+    public long getLastMessageUID(String email, MailProvider emailProvider, String token) {
+        Properties properties = emailSessionPropertiesService.getSessionProperties(emailProvider.getConfigurationName());
 
-    public long getLastMessageUID(String email, String emailProvider, String token) {
-        Properties properties = emailSessionPropertiesService.getSessionProperties(emailProvider);
         return emailSessionService.getLastMessageUID(properties, email, token);
     }
 
     public String getMessageByUID(long userEmailId, long messageUID) {
-        UserEmailEntity userEmail = userEmailRepository.getReferenceById(userEmailId);
-        Properties properties = emailSessionPropertiesService.getSessionProperties(userEmail.getEmailProvider());
+        UserEmailPostgreEntity userEmail = userEmailPostgreService.getUserEmail(userEmailId);
+        Properties properties = emailSessionPropertiesService.getSessionProperties(
+                userEmail.getMailProvider().getConfigurationName()
+        );
+
+        if (Instant.now().isAfter(userEmail.getEndAccessTokenLife())) {
+            authenticationService.refreshToken(userEmail.getId(), true);
+        }
 
         EmailMessage message = emailSessionService.getMessageByUID(
                 properties,
