489372ce53768b7e521549266658d22221a913f1
vyacheslavnov03 <vyacheslavnov03@mail.ru>
2024-11-25 19:55:00 +0300

    corrected the formatting

--- src/main/java/ru/tbank/emailcheckerbot/integration/http/yandex/YandexClient.java
+++ src/main/java/ru/tbank/emailcheckerbot/integration/http/yandex/YandexClient.java
@@ -5,9 +5,10 @@ import ru.tbank.emailcheckerbot.dto.token.RefreshTokenResponseDTO;
 import ru.tbank.emailcheckerbot.dto.yandex.YandexUserInfoDTO;
 
 public interface YandexClient {
-    YandexUserInfoDTO getUserInfo(String token);
 
     AccessTokenResponseDTO getAccessToken(String code);
 
     RefreshTokenResponseDTO refreshAccessToken(String refreshToken);
+
+    YandexUserInfoDTO getUserInfo(String token);
 }
