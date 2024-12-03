1323bf36239a20299907b0b49b481d874c0db114
vyacheslavnov03 <vyacheslavnov03@mail.ru>
2024-12-04 01:51:19 +0300

    corrected the redis configuration

--- src/main/java/ru/tbank/emailcheckerbot/configuration/redis/RedisConfiguration.java
+++ src/main/java/ru/tbank/emailcheckerbot/configuration/redis/RedisConfiguration.java
@@ -2,6 +2,7 @@ package ru.tbank.emailcheckerbot.configuration.redis;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -10,7 +11,8 @@ public class RedisConfiguration {
 
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("redis", 6379);
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
